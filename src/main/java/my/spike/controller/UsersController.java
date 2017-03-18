93d66787f2c9a78f5ba55e5dc2f4cfa77d212244
Ajay Divakaran <ajay_886@hotmail.com>
2017-03-18 16:11:46 +0530

    Updated code

--- src/main/java/my/spike/controller/UsersController.java
+++ src/main/java/my/spike/controller/UsersController.java
@@ -1,6 +1,7 @@
 package my.spike.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -9,7 +10,8 @@ import java.util.List;
 @RestController
 public class UsersController {
     @GetMapping("/users")
-    public List<String> getUsers() {
+    public List<String> getUsers(@RequestHeader("userid") String userId) {
+        System.out.println("Endpoint validated for user:" + userId);
         return Arrays.asList("user1", "user2");
     }
 }
